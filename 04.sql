6f91b8b99b7cb6837c008878fa4b032d00377490
Caio Rodrigues <rfcaio90@gmail.com>
2019-08-05 20:12:42 -0300

    fix `AS` keyword

--- 04.sql
+++ 04.sql
@@ -1,8 +1,8 @@
 
-SELECT AVG(price) as Average FROM purchase WHERE purchase_date < '2018-05-12';
+SELECT AVG(price) AS Average FROM purchase WHERE purchase_date < '2018-05-12';
 
-SELECT payment_way as `Payment Way`, SUM(price) as Total FROM purchase WHERE purchase_date < '2018-10-10' GROUP BY payment_way;
+SELECT payment_way AS `Payment Way`, SUM(price) AS Total FROM purchase WHERE purchase_date < '2018-10-10' GROUP BY payment_way;
 
-SELECT COUNT(*) as `Count` FROM purchase WHERE purchase_date < '2018-05-12' AND received = 1;
+SELECT COUNT(*) AS `Count` FROM purchase WHERE purchase_date < '2018-05-12' AND received = 1;
 
-SELECT payment_way as `Payment Way`, received as Received, SUM(price) as Total FROM purchase GROUP BY payment_way, received;
+SELECT payment_way AS `Payment Way`, received AS Received, SUM(price) AS Total FROM purchase GROUP BY payment_way, received;
